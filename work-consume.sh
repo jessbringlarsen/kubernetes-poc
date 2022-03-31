80490e7fad2e2d8f0aed29dcfdeb620e54dc2a4d
yco2 <yco2@nykredit.dk>
2022-03-31 10:16:45 +0200

    Fix queue name

--- work-consume.sh
+++ work-consume.sh
@@ -2,4 +2,4 @@
 
 export BROKER_URL=amqp://guest:guest@rabbitmq-service:5672
 
-/usr/bin/amqp-consume --url=$BROKER_URL -q foo -c 1 cat && echo
+/usr/bin/amqp-consume --url=$BROKER_URL -q job1 -c 1 cat && echo
