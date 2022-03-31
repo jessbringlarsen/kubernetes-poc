c91802bbfed542c0085db0ca2b4e44946c8304c8
yco2 <yco2@nykredit.dk>
2022-03-31 10:08:32 +0200

    Changed to javase

--- src/main/java/dk/commentor/kubernetes/poc/Application.java
+++ src/main/java/dk/commentor/kubernetes/poc/Application.java
@@ -1,14 +1,6 @@
 package dk.commentor.kubernetes.poc;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
-
 
 class Application {
 
--- src/main/java/dk/commentor/kubernetes/poc/JobCommand.java
+++ src/main/java/dk/commentor/kubernetes/poc/JobCommand.java
@@ -1,16 +1,25 @@
 package dk.commentor.kubernetes.poc;
 
+import io.kubernetes.client.PodLogs;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.Streams;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
 @Command(name = "job", description = "Show Kubernetes jobs", mixinStandardHelpOptions = true)
 public class JobCommand implements Callable<Integer> {
-
-    private String job;
-    private String pod;
 
     @Override
     public Integer call() {
@@ -20,18 +29,34 @@ public class JobCommand implements Callable<Integer> {
 
     @Command(name = "show", description = "Show stats about a Kubernetes jobs")
     public void show(@Option(names = {"--job"}, description = "Name of job", required = true) String job) {
-        this.job = job;
-        System.out.println(job + " stats...");
     }
 
     @Command(name = "logs", description = "Prints logs from a pod")
-    public void logs(@Option(names = "--pod", description = "Name of pod", required = true) String pod) {
-        this.pod = pod;
-        System.out.println(String.format("%s logs: %s", pod, "logs..."));
+    public void logs(@Option(names = "--pod", description = "Name of pod", required = true) String pod) throws Exception {
+
+        PodLogs logs = new PodLogs();
+        List<V1Pod> pods = getApi().listNamespacedPod("default", "false", null, null, null, null, null, null, null, null, null).getItems();
+        Optional<V1Pod> v1Pod = pods.stream()
+            .filter(p -> p.getMetadata().getName().equals(pod))
+            .findFirst();
+
+        if (v1Pod.isEmpty()) {
+            System.out.println("Pod not found: " + pod);
+            System.exit(1);
+        }
+
+        InputStream is = logs.streamNamespacedPodLog(v1Pod.get());
+        Streams.copy(is, System.out);
+    }
+
+    private CoreV1Api getApi() throws IOException {
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+        return new CoreV1Api(client);
     }
 
     @Command(name = "pods", description = "Prints pods in job")
-    public void pods() {
-        System.out.println("pods...");
+    public void pods(@Option(names = {"--job"}, description = "Name of job", required = true) String job) throws Exception {
+
     }
 }
