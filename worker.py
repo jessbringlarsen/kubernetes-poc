063c45ece2a50f075023757194434f1eabf6b557
Jess Bring-Larsen <jess@bring-larsen.dk>
2022-04-06 14:30:09 +0200

    Updated for fine processing

--- worker.py
+++ worker.py
@@ -2,15 +2,24 @@
 
 import sys, time, pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-service'))
-channel = connection.channel()
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-service'))
+    channel = connection.channel()
 
-channel.basic_consume(queue='job1',
-                      auto_ack=True,
-                      on_message_callback=callback)
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    time.sleep(10)
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        time.sleep(10)
 
-channel.start_consuming()
+    channel.basic_consume(queue='job1', auto_ack=True, on_message_callback=callback)
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
