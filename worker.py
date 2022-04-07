8eb51fe081be9042d40fc7a5bb1cc9069ea663d9
Jess Bring-Larsen <jess@bring-larsen.dk>
2022-04-07 21:39:01 +0200

    Adjust for example

--- worker.py
+++ worker.py
@@ -5,9 +5,16 @@ import sys, time, pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-service'))
 channel = connection.channel()
 
-method_frame, header_frame, body = channel.basic_get('job1')
-if method_frame:
-    print(method_frame, header_frame, body)
-    channel.basic_ack(method_frame.delivery_tag)
-else:
-    print('No message returned')
+for i in range(3):
+    time.sleep(5)
+    method_frame, header_frame, body = channel.basic_get('job1')
+    if method_frame:
+        print(method_frame, header_frame, body)
+        channel.basic_ack(method_frame.delivery_tag)
+        if 'fig' in body.decode("utf-8"):
+            print('Failure!')
+            sys.exit(1)
+    else:
+        print('No message returned')
+
+connection.close()
