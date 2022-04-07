dfdc77ad87de1d2e3cf0862da8035d884bdee883
Jess Bring-Larsen <jess@bring-larsen.dk>
2022-04-07 21:28:26 +0200

    Adjust for example

--- work-create.sh
+++ work-create.sh
@@ -2,7 +2,7 @@
 
 export BROKER_URL=amqp://guest:guest@rabbitmq-service:5672
 
-for f in apple banana cherry date fig grape lemon melon
+for f in fig apple banana cherry date grape
 do
   /usr/bin/amqp-publish --url=$BROKER_URL -r job1 -p -b $f
 done
